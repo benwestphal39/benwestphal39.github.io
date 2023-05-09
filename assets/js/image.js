aabafe1a62ab3fa4a7153ac54dfa17fd08c11820
BonnieDev <benwestphal39@hotmail.com>
2023-05-09 18:37:55 +1000

    major update

--- assets/js/image.js
+++ assets/js/image.js
@@ -1,9 +1,16 @@
 const silhouette = document.getElementById('silhouette');
-const lightImage = silhouette.getAttribute('data-light');
-const darkImage = silhouette.getAttribute('data-dark');
+const welcome = document.getElementById('welcomeimg');
+
+const lightSilhouette = silhouette.getAttribute('data-light');
+const darkSilhouette = silhouette.getAttribute('data-dark');
+
+const lightWelcome = welcome.getAttribute('data-light');
+const darkWelcome = welcome.getAttribute('data-dark');
 
 if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-  silhouette.src = darkImage;
+  silhouette.src = darkSilhouette;
+  welcome.src = darkWelcome;
 } else {
-  silhouette.src = lightImage;
+  silhouette.src = lightSilhouette;
+  welcome.src = lightWelcome;
 }
--- assets/js/langctl.js
+++ assets/js/langctl.js
@@ -4,28 +4,28 @@ $(document).ready(function() {
 
     //  SET LANGUAGES ON LOAD
     if($.cookie("lang") == 0000){
-        window.location.href = "https://benwestphal39.github.io/lang/en";
+        window.location.href = "/lang/en";
     }
      
     //  SET LANG MANUAL
     $("#en").on('click',function(){
         $.cookie("lang", 1);
-        window.location.href = "https://benwestphal39.github.io/lang/en";
+        window.location.href = "/lang/en";
     });
 
     $("#jp").on('click',function(){
         $.cookie("lang", 2);
-        window.location.href = "https://benwestphal39.github.io/lang/jp";
+        window.location.href = "/lang/jp";
     });
 
     $("#zh").on('click',function(){
         $.cookie("lang", 3);
-        window.location.href = "https://benwestphal39.github.io/lang/zh";
+        window.location.href = "/lang/zh";
     });
 
     $("#no").on('click',function(){
         $.cookie("lang", 4);
-        window.location.href = "https://benwestphal39.github.io/lang/no";
+        window.location.href = "/lang/no";
     });
 
 });
